7b9c228d77599dce4555b18444a8375f7416ee92
samuelcarneirosilveira <samuelsilveira14@gmail.com>
2024-07-10 11:19:08 -0300

    trash_hold_vl

--- src/examples/VL53/VL53_sensors.h
+++ src/examples/VL53/VL53_sensors.h
@@ -9,15 +9,27 @@
 #define SDIST_4 19
 #define NUM_SENSORS 4
 
+
+//#define B1B_K_1_Led
+#define B1B_K_3_Led
+
 class VL53_sensors
 {
 private:
-    /* data */
+    /* 0 - Direita */
 public:
     int number_sensor = NUM_SENSORS; // change this value 
     int x_shut_pins[NUM_SENSORS] = {SDIST_1, SDIST_2, SDIST_3, SDIST_4} ; 
     int dist[NUM_SENSORS];
-    int offset[NUM_SENSORS] = {10, 0, 40, 0};
+
+        // Com trash holder 
+    #ifdef B1B_K_3_Led
+        int offset[NUM_SENSORS] = {-62, -23, 0, 0}; //             
+    #endif
+
+    #ifdef B1B_K_1_Led
+        int offset[NUM_SENSORS] = {0, -31, 0, -20}; // 
+    #endif
 
     VL53L0X sensor[NUM_SENSORS];
     
